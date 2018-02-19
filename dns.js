954ad213bcb976d774eb4dc219937c1390c4b9d8
Jose <jose@localhost.localdomain>
2018-02-18 20:35:42 -0500

    º

--- dns.js
+++ dns.js
@@ -1,6 +1,21 @@
 var dnsd = require('dnsd')
+let listaNegra=["xvideos.com,redtube.com,pornhub.com"]
+
+
 dnsd.createServer(function(req, res) {
-  res.end('127.0.0.1')
+    console.log(req.question)
+    let domainName=req.question[0].name
+    if(listaNegra.indexOf(domainName) >= 0){
+        res.end('127.0.0.1')
+    }else{
+        /*dns.resolve4(domainName, function (err, addresses) {
+            if(addresses){
+                console.log('Registros A');
+                console.log(addresses[0]);
+            }
+        })*/
+        res.end('127.0.0.1')
+    }
 }).listen(53, '127.0.0.1')
 
 console.log('Server running at 127.0.0.1:53')
