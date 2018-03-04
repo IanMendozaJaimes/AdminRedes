93a082b52ed3142ecfcfe51debd5b963573c7365
Ian Mendoza Jaimes <ian@ianonsio.ianonsio.com>
2018-03-03 20:39:22 -0600

    analizar trama dhcp

--- DHCP/servidor_dhcp.c
+++ DHCP/servidor_dhcp.c
@@ -4,13 +4,15 @@
 int main(){
 	
 	struct sockaddr_in addr;
+	struct respuesta res;
+	struct mensaje_info msg_info;
 	int servidor, leidos;
 	socklen_t addrlen;
 
-	servidor = iniciar_servidor(GRUPO_BROADCAST, PUERTO_CLIENTE, &addr);
+	servidor = iniciar_servidor(GRUPO_BROADCAST, PUERTO_SERVIDOR, &addr);
 	addrlen = sizeof(addr);
 	
-	printf("El servidor esta listo y escuchando en el puerto %d.\n\n", PUERTO_CLIENTE);
+	printf("El servidor esta listo y escuchando en el puerto %d.\n\n", PUERTO_SERVIDOR);
 	
 	while(1){
 
@@ -19,14 +21,48 @@ int main(){
 		if((leidos = recvfrom(servidor, buffer, MAX_BUFFER, 0, (struct sockaddr*) &addr, &addrlen)) < 0){
 			error("No se pudo recibir el mensaje");
 		}
-
-		imprimir_trama(buffer, leidos);
 		
+		imprimir_trama(buffer, leidos);
+		analizar_mensaje(&msg_info, buffer, leidos);
+		generar_cabecera_respuesta(&res, buffer, &msg_info);		
+		imprimir_trama(res.contenido, res.tam);		
 	}
 
 	return 0;
 }
 
+
+int analizar_mensaje(struct mensaje_info * info, unsigned char * msg, int tam){
+	if(info == NULL){return -1;}
+	if(msg == NULL){return -1;}
+	
+	// obtenemos el hardware address
+	info -> haddr_len = msg[2];
+
+	info -> haddr = (unsigned char *)malloc(info -> haddr_len * sizeof(char));
+	info -> ip = (unsigned char *)calloc(4,  sizeof(char));
+	info -> tid = (unsigned char *)malloc(4 * sizeof(char));
+
+	// obtenemos el transaction id
+	copiar_arreglo(info -> tid, msg + 4, 4);
+
+	// obtenemos la direccion fisica
+	copiar_arreglo(info -> haddr, msg + 28, info -> haddr_len);
+
+	// ya con su direccion fisica, podemos ver si tenemos una ip asignada
+	// a esta maquina, esto tiene que hacerce con tablas en una base de datos
+	// que por cierto, aun no tenemos jeje, por eso lo vamos a hacer feo, y le
+	// daremos una ip ya fija
+	
+	// ====================================
+	info -> ip[0] = 0xc0;
+	info -> ip[1] = 0xa8;
+	info -> ip[2] = 0x01;
+	info -> ip[3] = 0x64;
+	// ====================================
+
+	return 0;
+}
 
 
 int iniciar_servidor(char * host, int puerto, struct sockaddr_in * addr){
@@ -65,6 +101,43 @@ int iniciar_servidor(char * host, int puerto, struct sockaddr_in * addr){
 
 	return sock;
 }
+
+int generar_cabecera_respuesta(struct respuesta * res, unsigned char * msg, struct mensaje_info * info){
+	if(res == NULL){return -1;}
+	
+	int t = 237; // la cabecera mide 236 pero tambien contammos el caracter de fin de cadena
+	res -> contenido = (unsigned char *)malloc(t*sizeof(char));
+	res -> tam = t - 1;
+
+	// op = 2, es respuesta
+	res -> contenido[0] = 0x02;
+	
+	// ponemos el htype, hlen, hops, xid, secs, flas y clien ip address como en el msg
+	copiar_arreglo(res -> contenido + 1, msg + 1, 15);
+	
+	// yiaddr, vamos a hacerlo feo, dando una ip fija
+	copiar_arreglo(res -> contenido + 16, info -> ip, 4);	
+	
+	// siaddr, giaddr, chaddr, padding del chaddr
+	copiar_arreglo(res -> contenido + 20, msg + 20, 152);
+
+	res -> contenido[236] = '\0';		
+
+	return 0;
+}
+
+
+void copiar_arreglo(unsigned char * a, unsigned char * b, int tam){
+	if(a == NULL){return;}
+	if(b == NULL){return;}
+	
+	int i = 0;
+	
+	for(; i < tam; i++){
+		a[i] = b[i];	
+	}
+}
+
 
 void imprimir_trama(unsigned char * trama, int tam){
 	printf("================================================\n");
--- DHCP/servidor_dhcp.h
+++ DHCP/servidor_dhcp.h
@@ -14,12 +14,28 @@
 	#define PUERTO_CLIENTE 68
 	#define GRUPO_BROADCAST "225.255.255.255"
 	#define MAX_BUFFER 1024
+	
+
+	struct respuesta{
+		unsigned char * contenido;
+		int tam;
+	};
+	
+	struct mensaje_info{
+		char haddr_len;
+		unsigned char * haddr; // hardware address, la mayoria del tiempo es la mac
+		unsigned char * ip;
+		unsigned char * tid; // transaction id
+	};
 
 
 	int iniciar_servidor(char*, int, struct sockaddr_in*);
+	int generar_cabecera_respuesta(struct respuesta *, unsigned char *, struct mensaje_info *);
+	int analizar_mensaje(struct mensaje_info *, unsigned char *, int);
+
 	void imprimir_trama(unsigned char*, int);
 	void error(char*);
-
+	void copiar_arreglo(unsigned char *, unsigned char *, int);
 
 
 #endif
